a2dc4c574bc9bfdae2a23e6e580ee05d7cb626d4
sayantansnl <sayantansanyal72@gmail.com>
2026-08-17 20:47:24 +0530

    Theme change

--- lua/config/diagnostics.lua
+++ lua/config/diagnostics.lua
@@ -1,5 +1,9 @@
 vim.diagnostic.config({
-	virtual_text = true,
+	virtual_text = false,
+	virtual_lines = {
+		current_line = true,
+		overflow = "wrap",
+	},
 	signs = true,
 	underline = true,
 	update_in_insert = true,
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,13 +1,27 @@
 return {
-	"shatur/neovim-ayu",
+	"catppuccin/nvim",
 	priority = 1000,
 	lazy = false,
 	opts = {},
 	config = function()
-		require("ayu").setup({
-			mirage = true,
-			terminal = true,
+		require("catppuccin").setup({
+			flavour = "auto",
+			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+				comments = { "italic" }, -- Change the style of comments
+				conditionals = {},
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+				operators = {},
+				-- miscs = {}, -- Uncomment to turn off hard-coded styles
+			},
 		})
-		vim.cmd([[colorscheme ayu-mirage]])
+		vim.cmd([[colorscheme catppuccin-macchiato]])
 	end,
 }
